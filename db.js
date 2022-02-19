9f4da541814635df4913bc7c2e2f63506a2107b2
ismael-back <ismaelsktcore9898@hotmail.com>
2022-02-19 11:57:44 -0300

    Deploy server-side

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory`;
 const proConfig = process.env.DATABASE_URL;
 const pool = new Pool({
   connectionString:
