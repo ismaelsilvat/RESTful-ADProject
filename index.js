6fbcb74c11b3f02858f6cb1d5e0998c39cc0501c
ismael-back <ismaelsktcore9898@hotmail.com>
2022-02-19 12:07:16 -0300

    Deploy server-side

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ app.use('/files', express.static(__dirname + '/files'));
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "client-side/build")))
-  }
-
+}
 
 app.get("/")
 
